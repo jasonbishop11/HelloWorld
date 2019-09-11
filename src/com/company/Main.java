ce5f7afa54f68b53530799eb1fb8fa1bff5ba575
Jason Bishop <jasonbishop11@gmail.com>
2019-09-11 12:27:13 -0400

    Changes from 9/11/19

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello my name is Jason");
-	System.out.println("test");
+        /*int x = 3; //Integer
+        double y = 6.5; // Decimal
+        String name = "Jane Doe"; //Text
+        int z = 4;
+
+        //System.out.println(x);
+        //int a = x+z; //Assignment
+        x=x+1;
+        //System.out.println(x);
+        //System.out.println(a);
+
+        int b = x-z;
+        int c = x*z;
+        int d = x/z;
+
+        System.out.println(b+ " " +c+"  " + d);
+
+        int x1 = 5;
+        int y1 = 3;
+        int z1 =x1/y1;
+        float a =x1/y1;
+        */
+        int x = 4;
+        int y = 6;
+        int a = 5;
+        int b = 2;
+
+        double z = y/x +a%y;
+        System.out.println(z);
+        x++;
+        y=y+x++;
+
+        System.out.println(x + " " + y);
+        
     }
 }
